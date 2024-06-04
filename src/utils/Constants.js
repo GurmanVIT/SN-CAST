599806db0bea6318e30822db833b5d7fac150d44
AnmolDang <anmoldang121@gmail.com>
2024-06-04 15:50:23 +0530

    latest

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,6 @@
 //BASE URL
-export const ApiBaseUrl = "https://bot.cryptokity.com/";
+// export const ApiBaseUrl = "https://bot.cryptokity.com/";
+export const ApiBaseUrl = "https://dev.memate.com.au/";
 
 //Api Names
 
@@ -11,7 +12,10 @@ export const ApiBaseUrl = "https://bot.cryptokity.com/";
 // export const depositApi = "api/userNew/deposit";
 // export const cancelOrderApi = "api/userNew/cancelOrderV1";
 // export const closeTradeApi = "api/userNew/closeTradeV1";
-export const loginApi = "api/userNew/login";
+
+//dev.memate.com.au/api/v1/m/login/
+export const loginApi = "api/v1/m/login/";
+// export const loginApi = "api/userNew/login";
 export const profileApi = "api/userNew/profile";
 export const updateProfileApi = "api/userNew/profile";
 export const getAllOpenOrdersApi = "api/userNew/getAllOpenOrders";
